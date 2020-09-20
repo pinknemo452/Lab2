4560e882107adf197d8e834e8033f188b16f2ec1
pinknemo452 <mishanay38@gmail.com>
2020-09-20 21:22:43 +0300

    first lab 2 commit

---
+++ Lab2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() {
+	int i;
+	char string[101];
+	printf("Enter string: ");
+	fgets(string, 101, stdin);
+	for (i = 0; i <= 100; i++) {
+		if (string[i] == ' ') {
+			if (string[i + 1] != ' ') {
+				printf("\n");
+			}
+		}
+		else if (string[i] == '\0') {
+			return 0;
+		}
+		else {
+			printf("%c", string[i]);
+		}
+	}
+	return 0;
+}
+
